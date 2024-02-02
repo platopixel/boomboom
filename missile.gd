ab57ad1f95441300b2368c327022d34b3108d78f
platopixel <platopixel@gmail.com>
2024-02-02 00:06:12 -0700

    make missile sticky

--- missile.gd
+++ missile.gd
@@ -2,8 +2,14 @@ extends RigidBody2D
 
 @export var impulse_strength = 500
 
+func _ready():
+	self.add_to_group("missile")
+
 func _on_body_entered(body):
 	print('body entered')
 
 func fire_missile(direction):
 	self.apply_central_impulse(direction * impulse_strength)
+
+func missile_hit():
+	pass
